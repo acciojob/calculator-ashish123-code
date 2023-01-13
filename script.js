893fdee115665872dfddd73ebf7c3cca5658f871
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-01-13 11:59:23 +0530

    calculator

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let input = document.getElementById("input");
+let numbers = document.getElementById("numbers");
+let operations = document.getElementById("operations");
+let ansButton = document.getElementById("ans");
+let clrButton = document.getElementById("clr");
+let dotButton = document.getElementById("dot");
+let current = "";
+let previous = "";
+let operator = "";
+
+numbers.addEventListener("click", function(event) {
+  let button = event.target;
+  current += button.textContent;
+  input.value = current;
+});
+
+operations.addEventListener("click", function(event) {
+  let button = event.target;
+  if (button.textContent === "=") {
+    previous = current;
+    current = "";
+    operator = "";
+  } else if (button.textContent === "Clear") {
+    current = "";
+    previous = "";
+  }
