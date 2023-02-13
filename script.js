2463088fa3925838d66ffbcac0390fc76223c70a
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-02-13 19:36:58 +0530

    cal

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 //your code here
-let input = document.getElementById("input");
-let numbers = document.getElementById("numbers");
-let operations = document.getElementById("operations");
-let ansButton = document.getElementById("ans");
-let clrButton = document.getElementById("clr");
-let dotButton = document.getElementById("dot");
-let current = "";
-let previous = "";
-let operator = "";
+const buttons = document.querySelectorAll("button");
+const input = document.querySelector("#input");
 
-numbers.addEventListener("click", function(event) {
-  let button = event.target;
-  current += button.textContent;
-  input.value = current;
+buttons.forEach(button => {
+  button.addEventListener("click", e => {
+    switch (e.target.id) {
+      case "block0":
+      case "block1":
+      case "block2":
+      case "block3":
+      case "block4":
+      case "block5":
+      case "block6":
+      case "block7":
+      case "block8":
+      case "block9":
+        input.value += e.target.textContent;
+        break;
+      case "plus":
+      case "minus":
+      case "multiply":
+      case "divide":
+        input.value += e.target.textContent;
+        break;
+      case "dot":
+        if (!input.value.includes(".")) {
+          input.value += ".";
+        }
+        break;
+      case "ans":
+        try {
+          input.value = eval(input.value);
+        } catch (error) {
+          input.value = error;
+        }
+        break;
+      case "clr":
+        input.value = "";
+        break;
+    }
+  });
 });
 
-operations.addEventListener("click", function(event) {
-  let button = event.target;
-  if (button.textContent === "=") {
-    previous = current;
-    current = "";
-    operator = "";
-  } else if (button.textContent === "Clear") {
-    current = "";
-    previous = "";
-  }
+
